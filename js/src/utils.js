fb327cecf933dea0148de8f6b10556a4302ff874
fangdc <fang_gco@163.com>
2017-06-05 22:04:26 +0800

    Site updated: 2017-06-05 22:04:25

--- js/src/utils.js
+++ js/src/utils.js
@@ -14,8 +14,7 @@ NexT.utils = NexT.$u = {
         var $imageWrapLink = $image.parent('a');
 
         if ($imageWrapLink.size() < 1) {
-	        var imageLink = ($image.attr('data-original')) ? this.getAttribute('data-original') : this.getAttribute('src');
-          $imageWrapLink = $image.wrap('<a href="' + imageLink + '"></a>').parent('a');
+          $imageWrapLink = $image.wrap('<a href="' + this.getAttribute('src') + '"></a>').parent('a');
         }
 
         $imageWrapLink.addClass('fancybox fancybox.image');
@@ -40,9 +39,8 @@ NexT.utils = NexT.$u = {
 
   lazyLoadPostsImages: function () {
     $('#posts').find('img').lazyload({
-      //placeholder: '/images/loading.gif',
-      effect: 'fadeIn',
-      threshold : 0
+      placeholder: '/images/loading.gif',
+      effect: 'fadeIn'
     });
   },
 
